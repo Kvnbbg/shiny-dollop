6faeff61e2a43bdebb08e384a260acf1e5b083f2
Kevin Marville <kevinmarville@gmail.com>
2024-03-16 00:06:37 +0100

    Refactor close button functionality and add advisory section to home page

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,18 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Check the user's language setting
-    var userLang = '{{ session["lang"] }}'; // Assuming you're passing the session lang variable to your template
-  
-    // Find the close button by its class
-    var closeButton = document.querySelector('.btn-close[data-bs-dismiss="alert"]');
-  
-    // Set the button's text based on the language
-    if (userLang === 'fr') {
-      closeButton.innerHTML = 'Fermé le conseil'; // Note: This might be 'Fermer le conseil' for correct French
-    } else {
-      closeButton.innerHTML = 'Close'; // Default to English
-    }
-  
-    // Optional: Adjust the button appearance or functionality as needed
-    closeButton.classList.add('custom-close'); // Apply custom class for styling if necessary
-  });
-  
+    // Listening for click event on the close button
+    document.querySelector('.btn-close[data-bs-dismiss="alert"]').addEventListener('click', function() {
+        // Hide the advisory section when close button is clicked
+        this.closest('.alert-dismissible').style.display = 'none';
+    });
+});
