47203a32ae97918e26d5d86d799be3ebd86ec2a7
Kevin Marville <kevinmarville@gmail.com>
2024-03-18 00:03:47 +0100

    Update CSS and HTML files

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Listening for click event on the close button
+    const movableDiv = document.getElementById("movableDiv");
+    movableDiv.addEventListener("click", function() {
+      const maxX = window.innerWidth - this.offsetWidth;
+      const maxY = window.innerHeight - this.offsetHeight;
+      const randomX = Math.floor(Math.random() * maxX);
+      const randomY = Math.floor(Math.random() * maxY);
+      this.style.left = randomX + "px";
+      this.style.top = randomY + "px";
+    });
     document.querySelector('.btn-close[data-bs-dismiss="alert"]').addEventListener('click', function() {
         // Hide the advisory section when close button is clicked
         this.closest('.alert-dismissible').style.display = 'none';
     });
-});
+  });
